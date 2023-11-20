bb86e8f76b789924e3ef8589397154bf517a8510
Verpul <nicenick@mail.ru>
2023-11-20 12:09:01 +0300

    Zero seconds OffsetTime fix

--- api-service/src/main/java/ru/verpul/util/CurrenciesRateUtil.java
+++ api-service/src/main/java/ru/verpul/util/CurrenciesRateUtil.java
@@ -1,13 +1,15 @@
 package ru.verpul.util;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class CurrenciesRateUtil {
 
     public static String convertMillisToDateTime(Long millis) {
-        OffsetDateTime localDateTime = OffsetDateTime.ofInstant(Instant.ofEpochMilli(millis),
-                ZoneId.systemDefault()).withNano(0);
-        return CommonUtil.getFormattedUpdateDate(localDateTime.toString());
+        String localDateTime = OffsetDateTime.ofInstant(Instant.ofEpochMilli(millis),
+                ZoneId.systemDefault()).format(DateTimeFormatter.ISO_DATE_TIME);
+
+        return CommonUtil.getFormattedUpdateDate(localDateTime);
     }
 
     public static boolean isDateTimeSuitableForUpdate() {
